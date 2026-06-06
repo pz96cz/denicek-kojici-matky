55210a2f8fe4bc73307dfcc66b7f14201568fcc4
pz96cz <patrik.zapletal@mbi.io>
2026-06-06 21:09:31 +0200

    chore: scaffold Xcode project (iOS 26.5+, portrait-only iPhone)

---
+++ Kojeni/Kojeni/KojeniApp.swift
@@ -0,0 +1,17 @@
+//
+//  KojeniApp.swift
+//  Kojeni
+//
+//  Created by Patrik Zapletal on 06.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct KojeniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
